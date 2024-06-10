e0219fadddcadfaca2701ffe1895467ec08b5857
Pedro Elias <pedro.echagas@souunit.com.br>
2024-06-10 17:18:47 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,215 @@
+const menu = document.querySelector("#menu")
+const cartBtn = document.querySelector("#cart-btn")
+const cartModal = document.querySelector("#cart-modal")
+const cartItemsContainer = document.querySelector("#cart-items")
+const cartTotal = document.querySelector("#cart-total")
+const checkoutBtn = document.querySelector("#checkout-btn")
+const closeModalBtn = document.querySelector("#close-modal-btn")
+const cartCounter = document.querySelector("#cart-count")
+const adressInput = document.querySelector("#adress")
+const adressWarn = document.querySelector("#adress-warn")
+
+let cart = [];
+
+// abrir o modal do carrinho
+cartBtn.addEventListener("click", function() {
+    updateCartModal();
+    cartModal.style.display = "flex" 
+   
+})
+
+//fechar o modal quando clicar fora
+
+cartModal.addEventListener('click', function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = 'none'
+    }
+})
+
+// fechar o modal quando clicar fechar
+closeModalBtn.addEventListener('click', function(event){
+        cartModal.style.display = 'none'
+})
+
+//declarando o nome e os valores dos lanches
+
+menu.addEventListener('click', function(event){
+    let parentButton = event.target.closest('.add-to-cart-btn')
+    if(parentButton){
+        const name = parentButton.getAttribute('data-name')
+        const price = parseFloat(parentButton.getAttribute('data-price'))
+        addToCart(name, price)
+        // adicionando no carrinho
+
+
+    }
+
+} )
+    
+
+// função para adicionar no carrinho
+function addToCart(name, price){
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        existingItem.quantity += 1;
+    } else{
+        cart.push({
+            name,
+            price,
+            quantity: 1
+        })
+    }
+
+    updateCartModal()
+}
+
+//atualiza o carrinho
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        console.log(item)
+        const CartItemElement = document.createElement("div");
+        CartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        CartItemElement.innerHTML = `
+           <div class="flex items-center justify-between">
+             <div class="font-medium">
+                <p class="font-medium">${item.name}</p>
+                <p>Qtd: ${item.quantity}</p>
+                <p calss="fonte-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+             </div>
+
+               <button class="remove-from-cart-btn" data-name="${item.name}">
+                    Remover
+                </button>
+             
+           </div>
+        `
+
+        total += item.price * item.quantity
+
+
+
+        cartItemsContainer.appendChild(CartItemElement)
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+}
+
+
+//função para remover o item do carrinho
+cartItemsContainer.addEventListener('click', function(event){
+    if(event.target.classList.contains('remove-from-cart-btn')){
+        const name = event.target.getAttribute('data-name')
+
+        removeItemCart(name);
+    }
+
+
+    function removeItemCart (name){
+        const index = cart.findIndex(item => item.name === name);
+
+        if(index !== 1){
+            const item = cart[index];
+
+            if(item.quantity > 1){
+                item.quantity -= 1;
+                updateCartModal();
+                return;
+            }
+
+            cart.splice(index, 1);
+            updateCartModal();
+        }
+    }
+})
+
+
+adressInput.addEventListener('input', function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        adressInput.classList.remove("border-red-500")
+        adressWarn.classList.add('hidden')
+    }
+})
+
+//finalizar pedido
+checkoutBtn.addEventListener('click', function(){
+
+
+ // pedido somente no horario de funcionamento
+    const  isOpen = checkRestaurantOpen();
+    if(!isOpen){
+        
+        Toastify({
+            text: "Ops, o restaurante está fechado!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "#ef4444",
+            }, 
+        }).showToast();
+
+        return;
+    }
+
+
+
+    if(cart.length === 0) return;
+    if(adressInput.value === ""){
+        adressWarn.classList.remove("hidden")
+        adressInput.classList.add("border-red-500")
+        return;
+    }
+
+
+    // enviar pedido
+    const cartItems = cart.map((item) => {
+        return (
+            `${item.name} Quantidade: (${item.quantity}) Preço: R$ ${item.price} |`
+        )
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = '5579998979980'
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${adressInput.value}`, '_blank')
+
+    cart = [];
+    updateCartModal()
+
+
+
+
+
+})
+
+// verificar a hora e manipular o card hoario
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+}
+
+const spanItem = document.querySelector('#date-span')
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove('bg-red-500');
+    spanItem.classList.add('bg-green-600')
+}else{
+    spanItem.classList.remove('bg-green-600')
+    spanItem.classList.remove('bg-red-500')
+}
